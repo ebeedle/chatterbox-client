e885b99828cdc39bcee5a4d0779370f98d117476
ebeedle <ebeedle@ucla.edu>
2017-04-08 19:08:52 -0700

    new edits april 8

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,176 +1,72 @@
-<<<<<<< HEAD
 
-var app = {};
-app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-app.init = function() {
-  this.fetch();
-  context = this;
-  $('#main').on('click', '.username', function() {
-  	context.handleUsernameClick(this)
-  });
-};
-app.send = function(message) {
-  $.ajax({
-  	type: 'POST',
-  	url: this.server,
-  	data: JSON.stringify(message),
-  	contentType: 'application/json',
-  	success: function(message) {
-  		$('#chats').append('<div>' + message + ' </div>')
-  	}
-  })
-};
-app.fetch = function(){
-  $.ajax({
-  	url: this.server,
-  	type: 'GET',
-  	contentType: 'application/json',	
-  	success: function(data) {
+var app = {
+  username: window.location.search.substr(10),
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  $message: $('#message'),
+  roomname: 'lobby',
+  $send: $('#send'),
+  
+  init: function() {
+    this.fetch();
+    context = this;
+    $('#main').on('click', '.username', function() {
+   	  context.handleUsernameClick()
+    });
+  },
+  send: function(message) {
+    $.ajax({
+  	  url: app.server,
+  	  type: 'POST',
+  	  data: JSON.stringify(message),
+  	  //contentType: 'application/json',
+  	  success: function(message) {
+  		//$('#chats').append('<div>' + message + ' </div>')
+  	  }
+    })
+  },
+  fetch: function(){
+    $.ajax({
+  	  url: app.server,
+  	  type: 'GET',
+  	  data: { order: '-creatAt' },
+  	  contentType: 'application/json',	
+  	  success: function(data) {
   		data.results.forEach(function(x) {
   			return app.renderMessage(x);
   		})
-  	}
-  })
-};
+  	 }
+    })
+  },
 
-app.clearMessages = function() {
-	$('#chats').children().remove();
-}
+  clearMessages: function() {
+	$('#chats').html('');
+  },
 
-app.renderMessage = function(data) {
-	$('#chats').append('<a href="#" class=username>' + data.username + ': ' + data.text + '</a>')
+  renderMessage: function(data) {
+	$('#chats').append('<a href="#" class=username>' + data.username + ': ' + data.text + '</a>');
+  },
 
-}
+  renderRoom: function(room) {
+	$('#roomSelect').append('<div>' + room + '</div>');
+  },
 
-app.renderRoom = function(room) {
-	$('#roomSelect').append('<div>' + room + '</div>')
-}
-
-app.handleUsernameClick = function() {
+  handleUsernameClick: function() {
 	$('.friends').append('')
-}
-=======
-// $(document).ready(function() {
-
-
-var app = {};
-app.init = function() {
-  this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-  
-  
-};
-app.send = function(message) {
-  var sendMessage = $.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
   },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-  return sendMessage;
-};
 
-// app.fetch = function() {
-//   var getMessage = $.get("http://parse.sfm8.hackreactor.com/chatterbox/classes/messages", function(message){
-//         return this.send(message);
-//     });
-//   return getMessage;
-app.fetch = function() {
-  var getMessage = $.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: this.server,
-  type: 'GET',
-  // data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message received');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-  return getMessage;
-};
+  handleSubmit: function() {
+	var message = {
+		username: app.username,
+        message: app.$message.val(),
+        roomname: app.roomname || 'lobby'
+    }
 
-//adding messages to the DOM
-app.renderMessage = function(message) {
-  var $chat = $('<div></div>');
-  $chat.text(message);
-  $('#chats').append($chat);
-}
-//deleting messages from the DOM
-app.clearMessages = function() {
-  $('#chats').children().remove();
+    app.send(message);
+
+    event.preventDefault();
 }
 
-//adding rooms to the DOM
-app.renderRoom = function(message) {
-  var $subRoom = $('<div></div>')
-  $subRoom.text(message)
-  $('#roomSelect').append($subRoom)
-}
-
-//add a friend upon clicking their username
-
-app.handleUsernameClick = function() {
-  var $friend = $('<div></div>')
-  $friend.text(message.username)
-  $('.friend').append($friend)
 };
-
-$('#main').on('click','.username', function() {console.log('works')}/*this.handleUsernameClick*/);
-
-
-app.handleSubmit = function() {
-  var $friend = $('<div></div>')
-  $friend.text(message.username)
-  $('.friend').append($friend)
-};
-
-$('#main').on('click','.username', function() {console.log('works')}/*this.handleUsernameClick*/);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> f1eb798194c320b80db9ffaf48977f67060f13db
 
 
 
